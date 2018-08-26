12fe07f1730f42117e967b3b76ef06ba8c45e861
hubinhua <test@test.com>
2018-08-26 16:17:49 +0800

    spring boot 2.0 适配

---
+++ fox-security-app/src/main/java/com/fox/security/app/config/AppSecurytConfig.java
@@ -0,0 +1,22 @@
+package com.fox.security.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class AppSecurytConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+}
+
---
+++ fox-security-app/src/main/java/com/fox/security/app/config/FoxAuthorizationServerConfig.java
@@ -0,0 +1,42 @@
+package com.fox.security.app.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+
+@Configuration
+@EnableAuthorizationServer
+public class FoxAuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+
+  @Autowired
+  private BCryptPasswordEncoder passwordEncoder;
+
+  @Autowired
+  private AuthenticationManager authenticationManager;
+
+
+  @Override
+  public void configure(final AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+    endpoints//
+        .authenticationManager(authenticationManager)//
+    ;
+  }
+
+  @Override
+  public void configure(final ClientDetailsServiceConfigurer clients) throws Exception {
+    clients//
+        .inMemory()//
+        .withClient("client")//
+        .secret(passwordEncoder.encode("secret"))//
+        .authorizedGrantTypes("password", "refresh_token")//
+        .scopes("read", "write")//
+    ;
+  }
+
+}
